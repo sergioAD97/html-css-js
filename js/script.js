72176e9f0a80b15b0f747e6aedebedca1fbd960e
sergioAD97 <serduli97@hotmail.com>
2023-10-14 00:28:01 -0500

    Upload first

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Te amo demasiado ')
+});
+
+
+
+function mostrarMensaje() {
+    const respuesta = document.getElementById("respuesta");
+    if (event.target.innerHTML === "Sí") {
+        respuesta.textContent = "¡Sí, quiero ser tu novia!";
+    } 
+}
+
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
